99ad73abc62b03b173f786f7f1ef5e7f26ed6acf
aibaljose <aibaljosej@gmail.com>
2024-11-08 08:36:56 +0530

    v1

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Festbook</title>
+</head>
+
+<body>
+    <div class="nav">
+        <div class="name">FestBook</div>
+        <div class="menu_list">
+            <ul>
+                <li>Home</li>
+                <li>Events</li>
+                <li>Upcoming</li>
+                <li>about us</li>
+            </ul>
+            <div class="btn">login</div>
+        </div>
+    </div>
+    <section class="header">
+
+        <div class="header_text">
+            <h1>Explore <br>Tech  Events<br> with us</h1>
+            <p>Immerse yourself in the future of gaming with our state-of-the-art gaming controller designed for Virtual Reality.</p>
+            <div class="btn-g">
+                <div class="btn2">Explore more</div>
+                <div class="btn2">Play Video</div>
+            </div>
+        </div>
+        <div class="header_img">
+        <div class="booking_card">
+        <div class="card">
+            <form action="">
+                <div class="inpt">
+                    <h6>Event date</h6>
+                    <input type="date" placeholder="Adimaly" class="in1" >
+                </div>
+                <div class="inpt">
+                    <h6>Location</h6>
+                    <input type="text" placeholder="Adimaly" class="in1">
+                </div>
+
+                
+               <button class="btn" type="submit">Check</button>
+            </form>
+        </div>
+    </div>
+        </div>
+
+    </section>
+
+
+</body>
+
+</html>
